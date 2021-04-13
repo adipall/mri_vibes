362a4893cc19c91546c9b8b1c49e5b1033a1523d
Adi Pall <ap103@duke.edu>
2021-04-13 11:48:17 -0400

    merged code, use "final_data_analysis.m" for clean code that works by switching out only two variables: fname and prominence. tested and working with both types of files we have encountered so far (Badea-type, Oscillator-type). OR if you prefer, just use the separate badea-type and non-badea type files. Up to you! Will be there at 5 to help if any issues should arise.

--- DAQ/accel_badea_4_7/demo_analyze_data_V1.m
+++ DAQ/accel_badea_4_7/demo_analyze_data_V1.m
@@ -66,18 +66,11 @@ semilogx(fFreqs(locs),F_shifted(locs),'o')
 for i = 1:size(locs,1)
     str_loc{i} = append(num2str(fFreqs(locs(i))),' Hz')
 end
-text(fFreqs(locs)+20,F_shifted(locs),str_loc)
+text(fFreqs(locs)+0,F_shifted(locs),str_loc)
 % text(locs+.02,peaks,num2str((1:numel(peaks))'))
 xlabel('Frequencies (Hz)')
 ylabel('Amplitude (V)')
 set(findall(gcf,'-property','FontSize'),'FontSize',14)
 xlim([0,500]) % modify this, if you think there may be data past 500 Hz
-
-% function peaks = plotter(x,y)
-% 
-% 
-%
-% end
-
 
 
